8ed5c248de24dc3cc1477b3a333dffec4bec7ede
Dilli_Rao <101472151+legendarycopyninja@users.noreply.github.com>
2023-01-04 11:33:57 +0530

    async/await

--- index.js
+++ index.js
@@ -10,51 +10,50 @@ let myScore = 0
 
  document.getElementById('new-deck').addEventListener('click',handleNewDeck)
 
-function handleNewDeck () {
+async function handleNewDeck () {
     drawCard.disabled = false
     computerScore = 0
     myScore = 0
     computerScoreEl.textContent = `Computer score: ${computerScore}`
     myScoreEl.textContent = `My score: ${myScore}`
-     fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/')
-     .then(response => response.json())
-     .then( dataCard => {
-        displaycount.textContent = `Remaining Count : ${dataCard.remaining}`
-         deckID = dataCard.deck_id
-         console.log(dataCard)
-         header.textContent = `Draw Cards! `
-        })
-    }
+    const response = await fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/')
+    console.log(response)
+    const dataCard = await response.json()
+    console.log(dataCard)
+    displaycount.textContent = `Remaining Count : ${dataCard.remaining}`
+    deckID = dataCard.deck_id
+    // console.log(dataCard)
+    header.textContent = `Draw Cards! `
+        
+}
 
-    
-const handleDrawCard = () => {
-        fetch(`https://www.deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`)
-        .then(response => response.json())
-        .then(data => {
-            displaycount.textContent = `Remaining Count : ${data.remaining}`
+drawCard.addEventListener('click',handleDrawCard)
+
+ async function handleDrawCard () {
+        const response = await fetch(`https://www.deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`)
+        const data = await response.json()
+        displaycount.textContent = `Remaining Count : ${data.remaining}`
            
-            cardsContainer.children[0].innerHTML = `
-                <img src=${data.cards[0].image} class="card" />`
+        cardsContainer.children[0].innerHTML = `
+            <img src=${data.cards[0].image} class="card" />`
                 
-            cardsContainer.children[1].innerHTML = `
-                <img src=${data.cards[1].image} class="card" />`
+        cardsContainer.children[1].innerHTML = `
+             <img src=${data.cards[1].image} class="card" />`
             
-                const winnerText = determineCardWinner(data.cards[0],data.cards[1])
-                header.textContent = winnerText
-                if (data.remaining === 0) {
-                    drawCard.disabled = true
-                    if (computerScore > myScore){
-                        header.textContent = `Computer Won the Game!`
-                    } else if (myScore < computerScore ) {
-                        header.textContent = `You Won the Game!`
-                    } else {
-                        header.textContent = `Game Draw`
-                    }
-                }
-        } )
+        const winnerText = determineCardWinner(data.cards[0],data.cards[1])
+        header.textContent = winnerText
+        if (data.remaining === 0) {
+             drawCard.disabled = true
+                if (computerScore > myScore){
+                     header.textContent = `Computer Won the Game!`
+                } else if (myScore < computerScore ) {
+                    header.textContent = `You Won the Game!`
+                } else {
+                     header.textContent = `Game Draw`
+                 }
+             }
     }
     
-drawCard.addEventListener('click',handleDrawCard)
 
 function determineCardWinner(card1 , card2) {
     const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
